be0ad335797c344bcfff49250843263e6ba46dde
Etan31 <tumbagatristanehron31@gmail.com>
2024-07-09 21:43:51 +0800

    added comments on the autologin.js

--- src/middlewares/autologin.js
+++ src/middlewares/autologin.js
@@ -1,7 +1,7 @@
 const { pool } = require("../../config/dbConfig");
 const passport = require('passport');
 
-
+// this will get the authToken to be used after login in 
 const autoLogin = (req, res, next) => {
   const authToken = req.cookies.auth_token;
   if (authToken) {
@@ -12,7 +12,7 @@ const autoLogin = (req, res, next) => {
       }
       if (user) {
         req.user = user;
-        req.isAuthenticated = () => true;
+        req.isAuthenticated = () => true; //to automatically logged in th user 
       } else {
         res.clearCookie('auth_token');
       }
