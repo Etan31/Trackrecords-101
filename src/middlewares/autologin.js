42735c078c2a1443f7531e719286a78855b35139
Etan31 <tumbagatristanehron31@gmail.com>
2024-07-09 21:40:20 +0800

    removed/added  comments

--- src/middlewares/autologin.js
+++ src/middlewares/autologin.js
@@ -2,28 +2,6 @@ const { pool } = require("../../config/dbConfig");
 const passport = require('passport');
 
 
-// const autoLogin = (req, res, next) => {
-//   const authToken = req.cookies.auth_token;
-//   if (authToken) {
-//     passport.deserializeUser(authToken, (err, user) => {
-//       if (err) {
-//         console.error(err);
-//         return next();
-//       }
-//       if (user) {
-//         req.user = user;
-//         req.isAuthenticated = () => true;
-//         res.redirect('/dashboard'); // Redirect to dashboard
-//       } else {
-//         res.clearCookie('auth_token');
-//         res.redirect('/'); // Redirect to login page
-//       }
-//       next();
-//     });
-//   } else {
-//     next();
-//   }
-// };
 const autoLogin = (req, res, next) => {
   const authToken = req.cookies.auth_token;
   if (authToken) {
