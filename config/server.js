0fb73bedbd39e21721cb42744eb7dd5aa0f7f67b
Etan31 <tumbagatristanehron31@gmail.com>
2024-07-10 23:27:30 +0800

    removed comments

--- config/server.js
+++ config/server.js
@@ -11,7 +11,6 @@ const { pool } = require("./dbConfig");
 const autoLogin = require('../src/middlewares/autoLogin');
 const initializePassport = require("./passportConfig");
 
-// const memoryStore = new session.MemoryStore();
 
 initializePassport(passport);
 
