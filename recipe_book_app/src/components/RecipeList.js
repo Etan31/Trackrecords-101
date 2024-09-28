7e27f347e7cc47ffadca43fc0903bfea6c28996f
Etan31 <tumbagatristanehron31@gmail.com>
2024-09-29 00:14:00 +0800

    Done

--- recipe_book_app/src/components/RecipeList.js
+++ recipe_book_app/src/components/RecipeList.js
@@ -1,69 +1,19 @@
-import react from 'React';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const RecipeList = ({recipes}) => {
-   <div>
-      {/* <div className="card-list">
-         <h1 className="recipeName">Classic Milk Tea</h1>
-         <p>Ingredients:</p>
-         <ul>
-            <li>2 tsp black tea leaves</li>
-            <li>1/2 cup hot water</li>
-            <li>1/2 cup milk</li>
-            <li>1 tbsp sugar</li>
-            <li>Ice cubes</li>
-         </ul>
-         <p>Instructions:</p>
-         <p>Brew the black tea in hot water for 5 minutes, strain, and mix with milk and sugar. Serve with ice cubes.</p>
-      </div>
-      <div className="card-list">
-         <h1 className="recipeName">Taro Milk Tea</h1>
-         <p>Ingredients:</p>
-         <ul>
-            <li>2 tbsp taro powder</li>
-            <li>1/2 cup milk</li>
-            <li>1/2 cup brewed black tea</li>
-            <li>1 tbsp sugar</li>
-            <li>Ice cubes</li>
-         </ul>
-         <p>Instructions:</p>
-         <p>Mix taro powder with brewed tea, add milk and sugar, then serve over ice.</p>
-      </div>
-      <div className="card-list">
-         <h1 className="recipeName">Thai Milk Tea</h1>
-         <p>Ingredients:</p>
-         <ul>
-            <li>2 tbsp Thai tea mix</li>
-            <li>1/2 cup hot water</li>
-            <li>1/2 cup condensed milk</li>
-            <li>Ice cubes</li>
-         </ul>
-         <p>Instructions:</p>
-         <p>Brew the Thai tea mix in hot water for 5 minutes, strain, then mix with condensed milk. Serve over ice.</p>
-      </div>
-      <div className="card-list">
-         <h1 className="recipeName">Matcha Milk Tea</h1>
-         <p>Ingredients:</p>
-         <ul>
-            <li>1 tsp matcha powder</li>
-            <li>1/2 cup hot water</li>
-            <li>1/2 cup milk</li>
-            <li>1 tbsp honey</li>
-            <li>Ice cubes</li>
-         </ul>
-         <p>Instructions:</p>
-         <p>Mix matcha powder with hot water until dissolved, then stir in milk and honey. Serve over ice.</p>
-      </div> */}
-
-         {recipes.map((recipe, index) => (
-            <div key={index}>
-               <h2>{recipe.title}</h2>
-               <img src={recipe.img} alt={recipe.title} />
-               <p>{recipe.description}</p>
-               <Link to={`/recipe/${recipe}`}>View Recipe</Link>
-            </div>
-         ))}
-   </div>
-}
+const RecipeList = ({ recipes }) => {
+  return (
+    <div>
+      {recipes.map((recipe, index) => (
+        <div key={index}>
+          <h2>{recipe.title}</h2>
+          <img src={recipe.image} alt={recipe.title} />
+          <p>{recipe.description}</p>
+          <Link to={`/recipe/${index}`}>View Recipe</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default RecipeList;
--- recipe_book_app/src/js/App.js
+++ recipe_book_app/src/js/App.js
@@ -1,12 +1,59 @@
-import './../css/App.css';
+// App.js
+import React, { useState } from 'react';
+import { useMemo } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import RecipeList from './../components/RecipeList';
+import RecipeDetails from './../components/RecipeDetails';
+import AddRecipe from './../components/AddRecipe';
+import SearchBar from './../components/SearchBar';
+import FavoriteRecipes from './../components/FavoriteRecipes';
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [favorites, setFavorites] = useState([]);
+
+  const addRecipe = (newRecipe) => {
+    setRecipes([...recipes, newRecipe]);
+  };
+
+  const filteredRecipes = useMemo(() => {
+    return recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [recipes, searchQuery]);
+
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/add-recipe">Add Recipe</Link>
+        <Link to="/favorites">Favorite Recipes</Link>
+      </nav>
+
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+
+      <Routes>
+        <Route
+          path="/"
+          element={<RecipeList recipes={filteredRecipes} />}
+        />
+        <Route
+          path="/recipe/:id"
+          element={<RecipeDetails recipes={recipes} />}
+        />
+        <Route
+          path="/add-recipe"
+          element={<AddRecipe addRecipe={addRecipe} />}
+        />
+        <Route
+          path="/favorites"
+          element={<FavoriteRecipes favorites={favorites} />}
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
