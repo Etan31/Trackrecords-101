a2a9574d69e5dca8382e3ff1fabeb3774c72c00b
Etan31 <tumbagatristanehron31@gmail.com>
2024-10-04 14:58:10 +0800

    Added a navbar sample

---
+++ v2-trackrecord/src/components/NavBar.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// function NavBar() {
+//     const pages = ['Dashboard', 'Transaction', 'Budget', 'Analytics', 'Alerts'];
+//     const otherPages = ['Help', 'Settings', 'Login'];
+
+//     const navLinks = pages.map(page => {
+//       return (
+//             //Change this later
+//         <a href={'/' + page}>
+//            &nbsp;{page}
+//         </a>
+
+//       )
+//     });
+
+//     return <nav>{navLinks}</nav>;
+// }
+// export default NavBar;
