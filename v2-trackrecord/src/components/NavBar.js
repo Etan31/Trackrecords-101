e4922f47214c470294c113364c4c0851a68b92c6
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2024-12-24 22:59:17 +0800

    add: add navbar to hide and display side menu.

--- v2-trackrecord/src/components/NavBar.js
+++ v2-trackrecord/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useLocation} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // icons
    import Logo from './../assets/img/ic-trackrecord.png';
@@ -12,58 +12,69 @@ import { Link, useLocation} from 'react-router-dom';
    import ICSettings from './../assets/img/icons/ic-settings.svg';
    import ICAccount from './../assets/img/icons/ic-account.svg';
 
-//
-
-function NavBar() {
-
-  const [activeLink, setActiveLink] = useState('/')
+function NavBar({ onMenuToggle, menuActive }) {
+  const [activeLink, setActiveLink] = useState('/');
 
   const pages = [
     { name: 'Dashboard', icon: ICHome, route: '/' },
     { name: 'Transaction', icon: ICTransaction, route: '/transaction' },
     { name: 'Budget', icon: ICBudget, route: '/budget' },
     { name: 'Analytics', icon: ICGraph, route: '/analytics' },
-    { name: 'Alerts', icon: ICNotif, route: '/alerts' }
+    { name: 'Alerts', icon: ICNotif, route: '/alerts' },
   ];
 
   const otherPages = [
     { name: 'Help', icon: ICHelp, route: '/help' },
-    { name: 'Settings', icon: ICSettings, route: '/settings' }
+    { name: 'Settings', icon: ICSettings, route: '/settings' },
   ];
 
-  // to change current navigation active
   const handleLinkClick = (path) => {
-    setActiveLink(path);  
+    setActiveLink(path);
   };
 
-  const renderNavItems = (items) =>
-    items.map((item) => (
-      <Link 
-      to={item.route}
-      key={item.name}
-      onClick={() => handleLinkClick(item.route)}>
-
-        <li className={activeLink === item.route ? 'active' : ''}>
-          <div className="ic">
-            <img src={item.icon} alt={`${item.name} icon`} />
-          </div>
-          <div className="nav-name">{item.name}</div>
-        </li>
-      </Link>
-    ));
+  const toggleMenu = () => {
+    onMenuToggle(!menuActive);
+  };
 
   return (
-    <nav>
+    <nav className={`nav ${menuActive ? 'nav-expanded' : 'nav-collapsed'}`}>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className="hamburger-bar"></div>
+        <div className="hamburger-bar"></div>
+        <div className="hamburger-bar"></div>
+      </div>
       <div className="logo">
         <Link to="/">
           <img src={Logo} alt="Track record logo" />
         </Link>
       </div>
       <div className="nav-items">
-        <ul>{renderNavItems(pages)}</ul>
+        <ul>
+          {pages.map((page) => (
+            <Link to={page.route} key={page.name} onClick={() => handleLinkClick(page.route)}>
+              <li className={activeLink === page.route ? 'active' : ''}>
+                <div className="ic">
+                  <img src={page.icon} alt={`${page.name} icon`} />
+                </div>
+                <div className="nav-name">{page.name}</div>
+              </li>
+            </Link>
+          ))}
+        </ul>
       </div>
       <div className="others">
-        <ul>{renderNavItems(otherPages)}</ul>
+        <ul>
+          {otherPages.map((page) => (
+            <Link to={page.route} key={page.name}>
+              <li>
+                <div className="ic">
+                  <img src={page.icon} alt={`${page.name} icon`} />
+                </div>
+                <div className="nav-name">{page.name}</div>
+              </li>
+            </Link>
+          ))}
+        </ul>
       </div>
       <div className="account">
         <Link to="/login">
