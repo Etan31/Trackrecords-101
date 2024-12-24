2cec13260cf76996aca440be5ae5b2930852b0ad
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2024-12-24 22:58:36 +0800

    add: navigation

--- v2-trackrecord/src/js/App.js
+++ v2-trackrecord/src/js/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import NavBar from '../components/NavBar'
+import NavBar from '../components/NavBar';
 import Transaction from './../pages/Transaction';
 import Budget from './../pages/Budget';
 import Analytics from './../pages/Analytics';
@@ -11,23 +11,38 @@ import Settings from './../pages/Settings';
 import Login from './../pages/Login';
 import DashboardContainer from '../pages/Dashboard';
 
-
 function App() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu2 = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
-    <div className="App dashboard">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/transaction" element={<Transaction />} />
-          <Route path="/budget" element={<Budget />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="/alerts" element={<Alerts />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<DashboardContainer />} /> {/* Main Dashboard */}
-        </Routes>
-      </Router>
+    <div className={`App ${menuActive ? 'menu-active' : ''}`}>
+      <div className="hamburger-menu2" onClick={toggleMenu2}>
+        <div className="hamburger-bar2"></div>
+        <div className="hamburger-bar2"></div>
+        <div className="hamburger-bar2"></div>
+      </div>
+
+      <div className="App dashboard">
+        <Router>
+          <div className={`nav ${menuActive ? 'nav-expanded' : ''}`}>
+          <NavBar onMenuToggle={setMenuActive} menuActive={menuActive} />
+          </div>
+          <Routes>
+            <Route path="/" element={<DashboardContainer menuActive={menuActive} />} />
+            <Route path="/transaction" element={<Transaction menuActive={menuActive} />} />
+            <Route path="/budget" element={<Budget />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/alerts" element={<Alerts />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
