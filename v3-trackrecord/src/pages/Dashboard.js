c58f717f62bfc79995e5fa554634c64574b61a8f
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2025-09-27 19:24:02 +0800

    feat: added icons and UI of dashboard

--- v3-trackrecord/src/pages/Dashboard.js
+++ v3-trackrecord/src/pages/Dashboard.js
@@ -1,96 +1,110 @@
 import OpenMenuButton from './../components/OpenMenuButton';
 import './../styles/Dashboard.css';
+import wallet_1 from './../assets/icons/ic-balance-wallet.svg'
+import wallet_2 from './../assets/icons/ic-income-wallet.svg'
+import arrow from './../assets/icons/ic-arrow.svg'
 
 function Dashboard({ isNavOpen, toggleNav }) {
   return (
     <section id="dashboard">
       <header className={isNavOpen ? 'header-open' : ''}>
         <div className='btn menu-btn'>
-          <OpenMenuButton isNavOpen={isNavOpen} toggleNav={toggleNav}/>
+          <OpenMenuButton isNavOpen={isNavOpen} toggleNav={toggleNav} />
         </div>
-        <div class="heading-text">
-          <h1>Good Morning, <span>Tristan</span> </h1> 
+        <div className="heading-text">
+          <h1>Good Morning, <span>Tristan</span> </h1>
           <p>Welcome to you financial insights.</p>
         </div>
-
-        <section className='level level1'>
-          <div class="panel total-balance">
-            <div class="left">
-              {/* <img src="" alt="wallet logo"/> */}
-              <p>Total balance</p>
-              <p>$123,456.00</p>
-            </div>
-            <div class="right">
-              <div class="daily-change">
-                {/* <img src="" alt="indicator icon"/> */}
-                <p>5%</p>
-              </div>
-              <div class="graph">
-              </div>
-            </div>
-
-          </div>
-          <div class="panel total-income">
-            <div class="left">
-              {/* <img src="" alt="wallet logo"/> */}
-              <p>Total Income</p>
-              <p>$20,491.00</p>
-            </div>
-            <div class="right">
-              <div class="daily-change">
-                {/* <img src="" alt="indicator icon"/> */}
-                <p>5%</p>
-              </div>
-              <div class="graph">
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className='level level2'>
-          <div class="panel finanicial-insights"></div>
-          <div class="panel notes"></div>
-        </section>
-
-        <section className='level level3'>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Date</th>
-                <th>Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Rent</td>
-                <td>31 May 2024 2:20 PM</td>
-                <td>&#8369;1,500</td>
-              </tr>
-              <tr>
-                <td>Rent</td>
-                <td>31 May 2024 2:20 PM</td>
-                <td>&#8369;1,500</td>
-              </tr>
-              <tr>
-                <td>Rent</td>
-                <td>31 May 2024 2:20 PM</td>
-                <td>&#8369;1,500</td>
-              </tr>
-              <tr>
-                <td>Rent</td>
-                <td>31 May 2024 2:20 PM</td>
-                <td>&#8369;1,500</td>
-              </tr>
-            </tbody>
-          </table>
-
-        </section>
-        <button>
-          {/* <img src="" alt="Add transaction"/> */}
-          Add Transaction
-        </button>
       </header>
+
+      <section className='level level1'>
+        <div className="panel total-balance">
+          <div className="left">
+            <img src={wallet_1} alt="wallet logo"/>
+            <h2>Total balance</h2>
+            <p>$123,456.00</p>
+          </div>
+          <div className="right">
+            <div className="daily-change">
+              <img src={arrow} alt="indicator icon"/>
+              <p>5%</p>
+            </div>
+            <div className="graph">
+            </div>
+          </div>
+
+        </div>
+        <div className="panel total-income">
+          <div className="left">
+            <img src={wallet_2} alt="wallet logo"/>
+            <h2>Total Income</h2>
+            <p>$20,491.00</p>
+          </div>
+          <div className="right">
+            <div className="daily-change">
+              <img src={arrow} alt="indicator icon"/>
+              <p>5%</p>
+            </div>
+            <div className="graph">
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='level level2'>
+        <h2 className="panel financial-insights">This area is financial</h2>
+        <h2 className="panel notes highlight">
+          This area is for the notes
+        </h2>
+      </section>
+
+      <section className='level level3 panel'>
+        <header className='level3-header'>
+          <h2>Recent Transaction</h2>
+          <div>
+            <p>View all</p>
+            {/* <img src="" alt="logo"/> */}
+          </div>
+        </header>
+        <table>
+          <title>Recent transaction</title>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Rent</td>
+              <td>31 May 2024 2:20 PM</td>
+              <td>&#8369;1,500</td>
+            </tr>
+            <tr>
+              <td>Rent</td>
+              <td>31 May 2024 2:20 PM</td>
+              <td>&#8369;1,500</td>
+            </tr>
+            <tr>
+              <td>Rent</td>
+              <td>31 May 2024 2:20 PM</td>
+              <td>&#8369;1,500</td>
+            </tr>
+            <tr>
+              <td>Rent</td>
+              <td>31 May 2024 2:20 PM</td>
+              <td>&#8369;1,500</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </section>
+      <button>
+        {/* <img src="" alt="Add transaction"/> */}
+        Add Transaction
+      </button>
+
 
     </section>
   );
